69bda89590893818442c34e1c83ef5afc9c533ac
Andrey Baryshev <as.bar.smr@gmail.com>
2021-03-02 08:21:27 +0400

    fix: predict.jst

--- src/predict.js
+++ src/predict.js
@@ -6,18 +6,33 @@ module.exports = async (image_path) =>{
   if (image_path) {
     opts.args = ['--fname', image_path]
   }
-  let pyshell = new PythonShell(`node_modules/captcha-breaker/src/predict.py`, opts );
-  const result = new Promise(resolve =>{
+  let pyshell = new PythonShell(`src/predict.py`, opts );
+
+  const resultPromise = new Promise(resolve =>{
     pyshell.on('message',  (message)=> {
+      console.log(message);
       if (message.includes('RESULT')) {
         const [,captchaText] = message.split('-');
         resolve(captchaText);
       }
     });
   })
-  pyshell.on('error', function (message) {});
-  const timeout = new Promise((resolve) => setTimeout(()=>resolve(null), 15000))
-  return Promise.race([result, timeout]);
+
+  let errorLog = [];
+  pyshell.on('error', function (message) {errorLog.push(message)});
+
+  const endPromise = new Promise (resolve=>{
+      pyshell.end( (err, code ) => {
+        if (code !== 0) {
+          errorLog.forEach(str=> console.log(str));
+          resolve(null);
+        }
+      })
+  })
+
+  const timeoutPromise = new Promise((resolve) => setTimeout(()=>resolve(null), 15000))
+
+  return Promise.race([resultPromise, timeoutPromise, endPromise]);
 }
 
 
